15eb6863dcd34813a06d989ec628ed8d4f161807
RyzenMu <muruga13in2022@gmail.com>
2025-10-05 08:50:04 +0530

    loaing spinner added

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,6 @@ import Header from "./components/Header"
 export default function App(){
   return <div className="bg-gray-500 text-white h-screen">
     <Header/>
-    <>Welcome to Seven Hills</>
+    <>Welcome to Seven Hills.</>
     </div>
 }
