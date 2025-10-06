dccfd85aa7dc01cff86a9489c7e61cd0f0d795d9
RyzenMu <muruga13in2022@gmail.com>
2025-10-07 04:07:59 +0530

    base name added 1

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -18,7 +18,7 @@ const Header: React.FC = () => {
     const data = await res.json();
     if (res.ok) {
       setAuthData(data.session.access_token, data.user.email);
-      navigate("/hero");
+      navigate("/SevenHills/hero");
     } else {
       alert(data.error || "Login failed");
     }
@@ -31,7 +31,7 @@ const Header: React.FC = () => {
       headers: { Authorization: `Bearer ${token}` },
     });
     setAuthData(null, null);
-    navigate("/");
+    navigate("/SevenHills");
   };
 
   return (
