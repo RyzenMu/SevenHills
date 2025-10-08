ca799215de1129561bed53728f64f128e2bf9108
RyzenMu <muruga13in2022@gmail.com>
2025-10-09 03:41:35 +0530

    modifed hero section 1

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -2,7 +2,6 @@ import React, { useContext, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { AuthContext } from "../context/AuthContext";
 import Tweet from "./Tweet";
-import type { text } from "stream/consumers";
 
 interface TweetType {
   id: number;
