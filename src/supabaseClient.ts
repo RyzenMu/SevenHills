17d7190a7168484ee3d18b9e9d6ef42688c62e35
RyzenMu <muruga13in2022@gmail.com>
2025-10-10 04:29:59 +0530

    supabasekey hard coded

--- src/supabaseClient.ts
+++ src/supabaseClient.ts
@@ -2,7 +2,7 @@ import { createClient } from "@supabase/supabase-js";
 import dotenv from 'dotenv';
 dotenv.config();
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
+const supabaseUrl = "https://vxexigrndbgzgbnbvhtr.supabase.co";
+const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZ4ZXhpZ3JuZGJnemdibmJ2aHRyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDM5MjE5NjksImV4cCI6MjA1OTQ5Nzk2OX0.hTWvkU7IRnkSG-kVJVnq6cdLK2Gn8w3nSj_IQXno-Qk";
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
