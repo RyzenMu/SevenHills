fc6ea5c851c6bdf8c6a89885447311407aaaefa0
RyzenMu <muruga13in2022@gmail.com>
2025-10-10 04:32:08 +0530

    removed dotenv code

--- src/supabaseClient.ts
+++ src/supabaseClient.ts
@@ -1,6 +1,5 @@
 import { createClient } from "@supabase/supabase-js";
-import dotenv from 'dotenv';
-dotenv.config();
+
 
 const supabaseUrl = "https://vxexigrndbgzgbnbvhtr.supabase.co";
 const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZ4ZXhpZ3JuZGJnemdibmJ2aHRyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDM5MjE5NjksImV4cCI6MjA1OTQ5Nzk2OX0.hTWvkU7IRnkSG-kVJVnq6cdLK2Gn8w3nSj_IQXno-Qk";
